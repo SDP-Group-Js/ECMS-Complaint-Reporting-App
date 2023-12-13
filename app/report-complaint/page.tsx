79999fbd9d2b186bda736a57c4f28decc413e228
meenu-methara <139040028+meenu-methara@users.noreply.github.com>
2023-12-13 21:05:49 +0530

    Starting work on report complaint UI

---
+++ app/report-complaint/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+
+const Header = () => {
+  return (
+    <div className="sticky top-0">
+      <NavBar />
+     
+    </div>
+  );
+};
+
+export default Header;
