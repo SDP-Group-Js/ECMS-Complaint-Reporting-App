43621aa21fef73182fd3e6ef6696ee51404aa01f
Sasvidu Ranthul <66816946+Sasvidu@users.noreply.github.com>
2023-12-21 04:47:28 +0530

    Add View Evidence Modal

--- app/components/ViewEvidenceButton.tsx
+++ app/components/ViewEvidenceButton.tsx
@@ -1,21 +1,30 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { IoMdOpen } from "react-icons/io";
+import ViewEvidenceModal from "./ViewEvidenceModal";
 
 type EvidenceButtonProps = {
   actionId: number;
 };
 
 const ViewEvidenceButton = ({ actionId }: EvidenceButtonProps) => {
-  //Hanlde button onclick TBA
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <>
-      <button className='flex h-12 w-44 items-center justify-center rounded-lg border-2 border-emerald-500 bg-emerald-500 px-4 py-2 text-sm text-white transition-all hover:font-bold lg:h-10 lg:w-48 lg:text-base'>
+      <button
+        className="flex h-12 w-44 items-center justify-center rounded-lg border-2 border-emerald-500 bg-emerald-500 px-4 py-2 text-sm text-white transition-all hover:font-bold lg:h-10 lg:w-48 lg:text-base"
+        onClick={() => setModalVisible(true)}
+      >
         <span>View Evidence</span>
         &nbsp;
         <IoMdOpen />
       </button>
+      <ViewEvidenceModal
+        isVisible={modalVisible}
+        actionId={actionId}
+        handleCloseButtonClick={() => setModalVisible(false)}
+      />
     </>
   );
 };
